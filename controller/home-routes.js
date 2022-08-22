6d2f89a27ce63c91134ad989abd840b4468b70c8
dcontrer83 <dcontrer83@yahoo.com>
2022-08-22 03:01:54 -0700

    Create routes for side ceteogrories and sides

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Sauce } = require('../model/Sauce');
+const { SidesCategory, Sides } = require('../model');
 
 // Render the homepage
 router.get('/', async (req, res) => {
@@ -25,10 +26,81 @@ router.get('/menu/sauces', async (req, res) => {
             sauce.get({ plain: true })
         });
         
-        res.render('menu', {sauces});
+        res.render('sauces', {sauces});
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// GET one sauce 
+router.get('/menu/sauces/:id', async (req, res) => {
+    try {
+        const dbSauceData = await Sauce.findByPk(req.params.id);
+
+        const sauce = dbSauceData.get({ plain: true });
+
+        res.render('sauce', { sauce });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// Render Sides Category page
+router.get('menu/sides', async (req, res) => {
+    try {
+        const dbSideCategoryData = await SidesCategory.findAll({
+            include: [
+                {
+                    model: Sides,
+                    attributes: ['title'],
+                },
+            ],
+        });
+
+        const sidesCategories = dbSideCategoryData.map((category) => {
+            category.get({ plain: true })
+        });
+
+        res.render('sidesCategories', { sidesCategories });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// GET one Side Category
+router.get('/menu/sides/:id', async (req, res) => {
+    try {
+        const dbSideCategoryData = await SidesCategory.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Sides,
+                    attributes: [
+                        'id',
+                        'title',
+                    ],
+                },
+            ],
+        });
+
+        const sideCategory = dbSideCategoryData.get({ plain: true });
+
+        res.render('sides', { sideCategory });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// GET one Side
+router.get('./menu/side/:id', async (req, res) => {
+    try {
+        const dbSideData = await Sides.findByPk(req.params.id);
+
+        const side = dbSideData.get({ plain: true });
+
+        res.render('side', { side });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
