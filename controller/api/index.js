42568cb05d3507705f2fa39977f009da91647069
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-23 10:40:25 -0700

    Added 'Choose your Wing Size' section to main menu page

--- controller/api/index.js
+++ controller/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const menuRoutes = require('./menu-routes');
 
-router.use('/menu', dishRoutes);
+// router.use('/menu', dishRoutes);
 
 module.exports = router;
