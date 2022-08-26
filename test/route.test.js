a31c440f27d41da3f58d368039562677e894510e
dcontrer83 <dcontrer83@yahoo.com>
2022-08-25 21:04:11 -0700

    Create tests GET, POST, PUT, and DELETE for Side Categories and Sides

--- test/route.test.js
+++ test/route.test.js
@@ -101,26 +101,109 @@ describe('Side Categories Routes', function() {
         expect(res.statusCode).toBe(200);
         expect(res.body.title).toBe('Dips');
         expect(res.body.sides[3].title).toBe('Fat Sauce');
-    });
+    })
 
-    //test POST request to create a Side Category
+    // test POST request to create a Side Category
     test('POST /api/menu/sidesCategory', async () => {
         const res = await request(app).post('/api/menu/sidesCategory').send(sidesCategoryData);
         const postId = res.body.id;
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200);
+        expect(res.body.title).toBe('Nachos');
         await request(app).delete(`/api/menu/sidesCategory/${postId}`);
+    })
+
+    // test PUT request to update a Side Category
+    test('PUT /api/menu/sidesCategory/:id', async () => {
+        const addItem = await request(app).post('/api/menu/sidesCategory').send(sidesCategoryData);
+        const postId = addItem.body.id;
+        const res = await request(app).put(`/api/menu/sidesCategory/${postId}`).send({
+            title: 'Nachos and guacamole'
+        });
+        const getItem = await request(app).get(`/api/menu/sidesCategory/${postId}`);
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+        expect(getItem.body.title).toBe('Nachos and guacamole');
+        await request(app).delete(`/api/menu/sidesCategory/${postId}`);
+    })
+
+    // test DELETE request to delete a Side Category
+    test('DELETE /api/menu/sidesCategory/:id', async () => {
+        const addItem = await request(app).post('/api/menu/sidesCategory').send(sidesCategoryData);
+        const postId = addItem.body.id;
+        const res = await request(app).delete(`/api/menu/sidesCategory/${postId}`);
+        expect(res.statusCode).toBe(200);
+        const getItems = await request(app).get('/api/menu/sidesCategory');
+        expect(res.body[postId]).toBe(undefined);
+        const res2 = await request(app).delete(`/api/menu/sidesCategory/${postId}`);
+        expect(res2.statusCode).toBe(404);
+        expect(res2.body.message).toBe('No side category found with that id!');
     })
 });
 
 // test route for side db
 describe('Side Routes', function() {
+    const sideData = {
+        title: "Milkshake (chocolate)",
+        cat_id: 6
+    }
+
+    // test GET request for all Sides
     test('responds to /api/menu/side', async () => {
         const res = await request(app).get('/api/menu/side');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200);
         expect(res.body[26].title).toBe('Hot Cheeto Mac');
         expect(res.body[26].cat_id).toBe(5);
+    })
+
+    // test GET request for one Side
+    test('GET /api/menu/side/:id', async () => {
+        const idCheck = 8;
+        const res = await request(app).get(`/api/menu/side/${idCheck}`);
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.title).toBe('Bleu Fries');
+        expect(res.body.cat_id).toBe(2);
+    })
+
+    // test POST request to create a Side
+    test('POST /api/menu/side', async () => {
+        const res = await request(app).post('/api/menu/side').send(sideData);
+        const postId = res.body.id;
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.title).toBe('Milkshake (chocolate)');
+        expect(res.body.cat_id).toBe(6);
+        await request(app).delete(`/api/menu/side/${postId}`);
+    })
+
+    // test PUT request to update a Side
+    test('PUT /api/menu/side/:id', async () => {
+        const addItem = await request(app).post('/api/menu/side').send(sideData);
+        const postId = addItem.body.id;
+        const res = await request(app).put(`/api/menu/side/${postId}`).send({
+            title: "Milkshake (chocolate or strawberry)",
+            cat_id: 5
+        });
+        const getItem = await request(app).get(`/api/menu/side/${postId}`);
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+        expect(getItem.body.title).toBe('Milkshake (chocolate or strawberry)');
+        expect(getItem.body.cat_id).toBe(5);
+        await request(app).delete(`/api/menu/side/${postId}`);
+    })
+
+    // test DELETE request to delete a Side
+    test('DELETE /api/menu/side/:id', async () => {
+        const addItem = await request(app).post('/api/menu/side').send(sideData);
+        const postId = addItem.body.id;
+        const res = await request(app).delete(`/api/menu/side/${postId}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body[postId]).toBe(undefined);
+        const res2 = await request(app).delete(`/api/menu/side/${postId}`);
+        expect(res2.statusCode).toBe(404);
+        expect(res2.body.message).toBe('No side found with that id!');
     })
 })
 
