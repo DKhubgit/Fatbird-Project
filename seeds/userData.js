c1ef20de47843491460b3386c400bebc80dd6167
DanielK <Danielkang13@gmail.com>
2022-08-25 21:38:37 -0700

    added user seed data for testing

---
+++ seeds/userData.js
@@ -0,0 +1,22 @@
+//NOTE: THIS IS ONLY FOR TESTING PURPOSES
+//NOT ACTUAL LOGIN INFO
+
+const sequelize = require("../config/config");
+const { User } = require("../model/User");
+
+const userData = [
+    {
+        username: "danielkang13",
+        password: "Strongpass",
+        is_admin: true,
+    }
+]
+
+async function create() {
+    await User.sync(); //creates the table, does not do anything if table exists
+    await User.create(userData[0]); //insert query to table
+    process.exit(0); //without this it keeps on running, must have.
+}
+
+create();
+
