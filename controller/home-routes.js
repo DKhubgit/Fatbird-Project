21071f5cff1662a38ebd082883db1f967400740d
dcontrer83 <dcontrer83@yahoo.com>
2022-08-30 04:55:03 -0700

    Create routes for creating, updateing, and deleteing for categories, sauces, and sides.

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -129,5 +129,136 @@ router.get('/login', async (req, res) => {
     res.render('login');
 })
 
+// render user homepage
+router.get('/user', async (req, res) => {
+    res.render('user');
+})
+
+// render create side category
+router.get('/user/createCategory', async (req, res) => {
+    res.render('createCategory');
+})
+
+// render update side category list
+router.get('/user/updateCategoryList', async (req, res) => {
+    try {
+        const dbSideCategoryData = await SidesCategory.findAll();
+        const sidesCategories = dbSideCategoryData.map((category) => 
+            category.get({ plain: true })
+        );   
+        res.render('updateCategoryList', { sidesCategories });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render update a side category
+router.get('/user/updateCategory/:id', async (req, res) => {
+    res.render('updateCategory');
+})
+
+// render delete side category list
+router.get('/user/deleteCategoryList', async (req, res) => {
+    try {
+        const dbSideCategoryData = await SidesCategory.findAll();
+        const sidesCategories = dbSideCategoryData.map((category) => 
+            category.get({ plain: true })
+        );
+        res.render('deleteCategoryList', { sidesCategories });
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render delete a side category
+router.get('/user/deleteCategory/:id', async (req, res) => {
+    res.render('deleteCategory');
+})
+
+// render create a sauce 
+router.get('/user/createSauce', async (req, res) => {
+    res.render('createSauce');
+})
+
+// render update sauce list
+router.get('/user/updateSauceList', async (req, res) => {
+    try {
+        const dbSauceData = await Sauce.findAll();
+        const sauces = dbSauceData.map((sauce) => 
+            sauce.get({ plain: true })
+        );
+        res.render('updateSauceList', { sauces });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render update a sauce
+router.get('/user/updateSauce/:id', async (req, res) => {
+    res.render('updateSauce');
+})
+
+// render delete sauce list
+router.get('/user/deleteSauceList', async (req, res) => {
+    try {
+        const dbSauceData = await Sauce.findAll();
+        const sauces = dbSauceData.map((sauce) =>
+            sauce.get({ plain: true })
+        );
+        res.render('deleteSauceList', { sauces });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render delete a sauce
+router.get('/user/deleteSauce/:id', async (req, res) => {
+    res.render('deleteSauce');
+})
+
+// render create a side
+router.get('/user/createSide', async (req, res) => {
+    res.render('createSide');
+})
+
+// render update side list
+router.get('/user/updateSideList', async (req, res) => {
+    try {
+        const dbSideData = await Sides.findAll();
+
+        const side = dbSideData.map((sides) => 
+            sides.get({ plain: true })
+        );
+        res.render('updateSideList', { side });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render update a side
+router.get('/user/updateSide/:id', async (req, res) => {
+    res.render('updateSide');
+})
+
+// render delete side list
+router.get('/user/deleteSideList', async (req, res) => {
+    try {
+        const dbSideData = await Sides.findAll();
+
+        const side = dbSideData.map((sides) => 
+            sides.get({ plain: true })
+        );
+        res.render('deleteSideList', { side });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render delete a side
+router.get('/user/deleteSide', async (req, res) => {
+    res.render('deleteSide');
+})
+
 module.exports = router;
 
