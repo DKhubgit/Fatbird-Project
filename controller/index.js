f51253434237a7578d73b4945e47f27c9d03c595
DanielK <Danielkang13@gmail.com>
2022-08-29 18:30:01 -0700

    added login route to index.js

--- controller/index.js
+++ controller/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const loginRoutes = require('./login-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/admin', loginRoutes);
 
 module.exports = router;
