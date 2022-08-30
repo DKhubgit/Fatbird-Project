4e553134c9bb735a758fbfbd9a6ebfc3502c2dff
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-30 12:48:17 -0700

    Created template for update category page, added styles

--- public/js/update-side-category.js
+++ public/js/update-side-category.js
@@ -1,12 +1,12 @@
 
 async function newFormHandler(event) {
     event.preventDefault();
-    const categID = document.querySelector('#updateCategTitle').value;
+    const categID = document.querySelector('#updateCategID').value;
 
     const response = await fetch(`api/menu/sidesCategory/:id`, {
         method: 'PUT',
         body: JSON.stringify({
-            title,
+            id: categID
         }),
         headers: {
             'Content-Type': 'application/json',
